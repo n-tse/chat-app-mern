7e46f5c0f303d4520582fde06c0dfcb9da677a2c
Nick Tse <nicholashtse@gmail.com>
2023-05-18 16:11:50 -0400

    start building out home page; import react-icons for chat icon

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+import {BsChatDotsFill} from 'react-icons/bs';
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <Row>
+      <Col md={6} className='d-flex flex-direction-column align-items-center justify-content-center'>
+        <div>
+          <h1>Chat with your friends</h1>
+          <p>Log in or sign up to start chatting now!</p>
+          <LinkContainer to="/chat">
+            <Button variant='primary'>
+              Start Chatting
+              <BsChatDotsFill style={{marginLeft:5}}/>
+            </Button>
+          </LinkContainer>
+        </div>
+      </Col>
+    </Row>
   )
 }
 
