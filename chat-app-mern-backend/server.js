797f75e728c047471df9ab5b4a19e822017f96c5
Nick Tse <nicholashtse@gmail.com>
2023-05-18 11:43:54 -0400

    add/build server.js file

---
+++ chat-app-mern-backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const rooms = ['general', 'work', 'fun/games', 'random'];
+const cors = require('cors');
+
+app.use(express.urlencoded({extended: true})); // 'extended: true' for receiving data from frontend
+app.use(express.json());
+app.use(cors()); // allows front and back end to communicate
+
+const server = require('http').createServer(app); // 'http' comes with node environment by default
+const PORT = 5001;
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+})
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+})
