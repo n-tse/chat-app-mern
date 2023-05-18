6134a2710cd8f58d2c2b77cc981a586aaacb5cca
Nick Tse <nicholashtse@gmail.com>
2023-05-18 14:01:50 -0400

    Link routes to pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <NavBar /> 
-      <h1>Chat App</h1>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/signup" element={<Signup />}/>
+        <Route path="/chat" element={<Chat />}/>
+      </Routes>
+      {/* <h1>Chat App</h1> */}
+    </BrowserRouter>
   );
 }
 
