446a4ae56f535dc74485166541eadd76ef3ac398
Nick Tse <nicholashtse@gmail.com>
2023-05-18 17:03:22 -0400

    add login page with bg image from unsplash

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col, Button, Form } from 'react-bootstrap'
+import "./css/Login.css"
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+      <Row>
+        <Col md={6} className="login__background"></Col>
+        <Col md={6} className='d-flex flex-direction-column align-items-center justify-content-center'>
+          <Form style={{width:"70%"}}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+  );
 }
 
-export default Login
+export default Login;
