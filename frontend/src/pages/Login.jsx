470dea305a223c4151e20f6a07bcbb8443a2a3be
Nick Tse <nicholashtse@gmail.com>
2023-05-19 16:16:01 -0400

    add state and form submission handling to login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,35 +1,53 @@
-import React from 'react';
-import { Row, Col, Button, Form } from 'react-bootstrap'
-import { Link } from 'react-router-dom';
-import "./css/Login.css"
+import React, { useState } from "react";
+import { Row, Col, Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./css/Login.css";
 
 function Login() {
-  return (
-      <Row>
-        <Col md={6} className="login__background"></Col>
-        <Col md={6} className='d-flex flex-direction-column align-items-center justify-content-center'>
-          <Form style={{width:"70%"}}>
-            <h1 className='text-center'>Log In</h1>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  })
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit" className='mt-1'>
-              Log In
-            </Button>
-            <div className='py-3'>
-              <p className='text-center'>
-                Don't have an account? <Link to="/signup">Sign Up</Link>
-              </p>
-            </div>
-          </Form>
-        </Col>
-      </Row>
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`logging in with credentials\nemail: ${formData.email}\npassword: ${formData.password}`)
+  }
+
+  return (
+    <Row>
+      {console.log(formData)}
+      <Col md={6} className="login__background"></Col>
+      <Col
+        md={6}
+        className="d-flex flex-direction-column align-items-center justify-content-center"
+      >
+        <Form style={{ width: "70%" }} onSubmit={handleSubmit}>
+          <h1 className="text-center">Log In</h1>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" name="email" value={formData.email} onChange={handleChange} required/>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" name="password" value={formData.password} onChange={handleChange} required/>
+          </Form.Group>
+          <Button variant="primary" type="submit" className="mt-1">
+            Log In
+          </Button>
+          <div className="py-3">
+            <p className="text-center">
+              Don't have an account? <Link to="/signup">Sign Up</Link>
+            </p>
+          </div>
+        </Form>
+      </Col>
+    </Row>
   );
 }
 
