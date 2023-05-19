483462a1032db58a76ca01020323a9092a0a3248
Nick Tse <nicholashtse@gmail.com>
2023-05-18 23:42:55 -0400

    complete default avatar display; add state to form to keep track of user input

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Button, Form } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import "./css/Signup.css"
 import defaultPicture from "../assets/default-avatar-profile-icon.jpg";
+import { BsCloudUploadFill } from 'react-icons/bs';
+
+const validateImg = () => {
+
+}
 
 function Signup() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  })
+
+  const handleChange = ({target}) => {
+    setFormData({...formData, [target.name]: target.value})
+  }
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    alert(`signed up with: name ${formData.name}, email ${formData.email}, password ${formData.password}`);
+  }
+
   return (
     <Row>
         <Col md={6} className="signup__background"></Col>
         <Col md={6} className='d-flex flex-direction-column align-items-center justify-content-center'>
-          <Form style={{width:"70%"}}>
+          <Form style={{width:"70%"}} onSubmit={handleSignup}>
               <h1 className='text-center'>Create an account</h1>
               <div className='signup-profile-picture-container'>
-                <img src={defaultPicture} className='signup-profile-picture' style={{width:20, height:20}}/>
+                <img src={defaultPicture} className='signup-profile-picture' />
+                <label htmlFor='image-upload' className='image-upload-label'><BsCloudUploadFill className='upload-picture-icon'/></label>
+                <input type='file' id='image-upload' hidden accept='image/png, image/jpeg, image/jpg' onChange={validateImg} />
               </div>
               <Form.Group className="mb-3" controlId="formBasicName">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your name" />
+              <Form.Control type="text" placeholder="Enter your name" name="name" value={formData.name} onChange={handleChange}/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control type="email" placeholder="Enter email" name="email" value={formData.email} onChange={handleChange}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" placeholder="Password" name="password" value={formData.password} onChange={handleChange}/>
             </Form.Group>
             <Button variant="primary" type="submit" className='mt-1'>
               Sign Up
