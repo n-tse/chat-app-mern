d9b3e584d96101c05eb668c276e5742f50b89f38
Nick Tse <nicholashtse@gmail.com>
2023-05-23 16:24:12 -0400

    add redux store to app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux"; // allows us to work with redux in our react application
+import { PersistGate } from "redux-persist/es/integration/react";
+import persistStore from "redux-persist/es/persistStore";
+import store from "./store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const persistedStore = persistStore(store);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistedStore}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
