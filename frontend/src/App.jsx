70040ce44716c18ad94587a627427cb684254f3a
Nick Tse <nicholashtse@gmail.com>
2023-05-24 12:04:39 -0400

    conditionally display page links based on login status

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Chat from "./pages/Chat";
+import { useSelector } from "react-redux";
 
 function App() {
+  const user = useSelector((state) => state.user);
   return (
     <BrowserRouter>
-      <NavBar /> 
+      <NavBar />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/signup" element={<Signup />}/>
-        <Route path="/chat" element={<Chat />}/>
+        <Route path="/" element={<Home />} />
+        {!user && (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </>
+        )}
+        <Route path="/chat" element={<Chat />} />
       </Routes>
       {/* <h1>Chat App</h1> */}
     </BrowserRouter>
