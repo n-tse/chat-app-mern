308c6ec5c2288eec5fcccbeeaf2b3ee6a76754a3
Nick Tse <nicholashtse@gmail.com>
2023-05-24 14:49:55 -0400

    add contextAPI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,24 +7,49 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Chat from "./pages/Chat";
 import { useSelector } from "react-redux";
+import { AppContext, socket } from "./context/appContext";
 
 function App() {
+  const [rooms, setRooms] = useState([]);
+  const [currentRoom, setCurrentRoom] = useState([]);
+  const [members, setMembers] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [privateMemberMsg, setPrivateMemberMsg] = useState({});
+  const [newMessages, setNewMessages] = useState({});
   const user = useSelector((state) => state.user);
+
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {!user && (
-          <>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </>
-        )}
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-      {/* <h1>Chat App</h1> */}
-    </BrowserRouter>
+    <AppContext.Provider
+      value={{
+        socket,
+        rooms,
+        setRooms,
+        currentRoom,
+        setCurrentRoom,
+        members,
+        setMembers,
+        messages,
+        setMessages,
+        privateMemberMsg,
+        setPrivateMemberMsg,
+        newMessages,
+        setNewMessages,
+      }}
+    >
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {!user && (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </>
+          )}
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
