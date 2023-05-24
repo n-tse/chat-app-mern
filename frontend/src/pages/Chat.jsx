8709a6c9485e1478043b7e56fad91a9bdfe1cb56
Nick Tse <nicholashtse@gmail.com>
2023-05-24 14:20:01 -0400

    conditionally render chat window based on login status

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import Sidebar from '../components/Sidebar';
 import MessageForm from '../components/MessageForm';
 import { Container, Row, Col } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 function Chat() {
+  const user = useSelector(state => state.user);
+  
   return (
     <Container>
       <Row>
         <Col md={4}>
+        {user &&
           <Sidebar />
+        }
         </Col>
         <Col md={8}>
           <MessageForm />
