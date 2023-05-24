63f54cfe54267c9ed008aad388e1d24908ce5bb2
Nick Tse <nicholashtse@gmail.com>
2023-05-24 10:24:20 -0400

    update navbar dropdown to display user's name and picture

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,37 +5,65 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { LinkContainer } from "react-router-bootstrap";
 import chatapplogo from "../assets/chatapplogo.png";
+import { useSelector } from "react-redux";
 
 function NavBar() {
+  // useSelector allows us to have access to our state
+  const user = useSelector((state) => state.user);
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <LinkContainer to="/">
           <Navbar.Brand>
-            <img src={chatapplogo} style={{ width: 60, height: 60, margin: 5 }} />
+            <img
+              src={chatapplogo}
+              style={{ width: 60, height: 60, margin: 5 }}
+            />
             My Chat App
           </Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <LinkContainer to="/login">
-              <Nav.Link>Log In</Nav.Link>
-            </LinkContainer>
+            {!user && (
+              <LinkContainer to="/login">
+                <Nav.Link>Log In</Nav.Link>
+              </LinkContainer>
+            )}
             <LinkContainer to="/chat">
               <Nav.Link>Chat</Nav.Link>
             </LinkContainer>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            {user && (
+              <NavDropdown
+                title={
+                  <>
+                    {user.name}{" "}
+                    <img
+                      src={user.picture}
+                      style={{
+                        height: 25,
+                        width: 25,
+                        objectFit: "cover",
+                        borderRadius: "50%",
+                      }}
+                    />
+                  </>
+                }
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
